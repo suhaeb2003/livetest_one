6b4effef40a8396d4ad220f94af0c9f458f0a900
suhaeb2003 <mdsuhaebh@gmail.com>
2024-07-08 23:18:44 +0600

    Live tast Done

---
+++ task_2.php
@@ -0,0 +1,6 @@
+<?php
+ $num1=55;
+ $num2=75;
+
+ $sum=$num1+$num2;
+ printf("The sum fo %d and %d is: %d",$num1,$num2,$sum);
---
+++ task_3.php
@@ -0,0 +1,7 @@
+<?php
+$age=30;
+if($age>=18 && $age < 65){
+    echo "Eligible";
+}else{
+    echo "Not Eligible";
+}
